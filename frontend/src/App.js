a1545ba5cb0ffca7df7e919e8432212292483f7a
KarolRogalski <46846302+KarolRogalski@users.noreply.github.com>
2022-05-30 16:08:33 +0100

    add routes home and product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,23 @@
 import data from './data'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div>
-      <header className='App-header'>
-        <a href='/'>BuyBug</a>
-      </header>
-      <main>
-        <h1> Featured Products</h1>
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img
-                  src={product.image}
-                  className='App-logo'
-                  alt={product.name}
-                />
-              </a>
-              <div className='product-info'>
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>£{product.price}</strong>
-                </p>
-                <button> Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header className='App-header'>
+          <Link to='/'>BuyBug</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
