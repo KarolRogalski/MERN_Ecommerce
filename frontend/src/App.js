e31bdf8b7c1484da1c1a37f0aa51e180a11a7a4c
KarolRogalski <46846302+KarolRogalski@users.noreply.github.com>
2022-05-30 14:01:43 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+       <a href="/">BuyBug</a>
+      </header>
+      <main>
+        list prodacts
+      </main>
+    </div>
+  );
+}
+
+export default App;
