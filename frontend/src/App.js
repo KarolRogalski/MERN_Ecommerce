9c992502bb249678573c05a8571b45bd86cb9a62
KarolRogalski <46846302+KarolRogalski@users.noreply.github.com>
2022-05-30 15:31:38 +0100

    add - products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
-
+import data from './data'
 
 function App() {
   return (
     <div>
-      <header className="App-header">
-       <a href="/">BuyBug</a>
+      <header className='App-header'>
+        <a href='/'>BuyBug</a>
       </header>
       <main>
-        list prodacts
+        <h1> Featured Products</h1>
+        <div className='products'>
+          {data.products.map((product) => (
+            <div className='product' key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img
+                  src={product.image}
+                  className='App-logo'
+                  alt={product.name}
+                />
+              </a>
+              <div className='product-info'>
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>£{product.price}</strong>
+                </p>
+                <button> Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
